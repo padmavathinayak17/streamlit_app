ec970081445bcd91f40257a581efcbb640bb0183
padmavathinayak17 <151005509+padmavathinayak17@users.noreply.github.com>
2023-11-17 11:06:52 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,6 @@ import streamlit
  
 streamlit.title('My Parents new Healthy Diner')
 streamlit.header('Breakfast Favorites')
-
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free-Range Egg')
